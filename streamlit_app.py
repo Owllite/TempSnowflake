163ab572e9e92ec136f58627a9e42bfac68d8ea6
you example <you@example.com>
2024-03-07 15:24:46 +1030

    Code Reorganise

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,21 @@ import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+
+# Setup Snowflake Connection
 cnx = st.connection("snowflake")
-# Write directly to the app
-# st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+session = cnx.session()
+
+# Get Snowflake data
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 # Get API Data
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
+
+#### Form Headers
 st.title("My Parents New Healthy Diner")
 
 st.write("Breakfast Menu")
@@ -27,19 +33,10 @@ st.write(
     """
 )
 
-#option = st.selectbox(
-#    'What is your favourite fruit?',
-#    ('Banana', 'Strawberries', 'Peaches'))
-
-#st.write('Your favourite fruit is:', option)
-
-session = cnx.session()
-
+#### Main Form Start
 customer_name = st.text_input('Name', placeholder='Type your name here')
 st.write('Your name is ',customer_name)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
@@ -52,9 +49,6 @@ if ingredients_list:
 else:
     st.session_state['submit_is_disabled'] = True
 
-#if 'submit_is_disabled' not in st.session_state:
-#    st.write('True')
-#    st.session_state['submit_is_disabled'] = True
     
 submit_button = st.button('Submit Order', 'submitorder',  disabled=st.session_state['submit_is_disabled'])
 
